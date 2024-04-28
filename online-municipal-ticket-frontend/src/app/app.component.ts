8991b903070d7405f9d39bd78557f32fb8fc96b5
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2024-04-28 12:20:08 +0200

    add login page

--- online-municipal-ticket-frontend/src/app/app.component.ts
+++ online-municipal-ticket-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginComponent } from './user/components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent],
+  imports: [RouterOutlet, TopBarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- online-municipal-ticket-frontend/src/app/top-bar/top-bar.component.ts
+++ online-municipal-ticket-frontend/src/app/top-bar/top-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user/services/user.service';
 import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, RouterLink],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
