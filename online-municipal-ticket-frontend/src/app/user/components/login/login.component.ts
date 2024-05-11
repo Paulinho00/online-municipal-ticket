dd2af8b5865fe1d55f0169cfee98417be7eb05dd
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2024-05-11 11:52:43 +0200

    Add wrong credentials erorr message to login page

--- online-municipal-ticket-frontend/src/app/user/components/login/login.component.ts
+++ online-municipal-ticket-frontend/src/app/user/components/login/login.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   readonly loginForm: FormGroup
+  public wrongCredentials: boolean = false;
   
   constructor(private readonly userService: UserService, 
     private readonly router: Router){
@@ -24,9 +26,14 @@ export class LoginComponent {
   onSubmit() {
     if(this.loginForm.valid){
       this.userService.login(this.loginForm.value)
-        .subscribe((response) => {
-          this.router.navigateByUrl('ticket-list');
-        });
+        .subscribe({
+          next: (response) => this.router.navigateByUrl('ticket-list'),
+          error: (e) => {
+            if(e.status == 401){
+              this.wrongCredentials = true;
+            }
+          }
+        })
     }
   }
 }
