3287e87466b9734bbe64504a88da6ad0d35d87dc
mic609 <michu-2001@o2.pl>
2024-05-20 00:43:04 +0200

    Added all tests for frontend

--- online-municipal-ticket-frontend/src/app/tickets/components/ticket-check/ticket-check.component.spec.ts
+++ online-municipal-ticket-frontend/src/app/tickets/components/ticket-check/ticket-check.component.spec.ts
@@ -7,7 +7,7 @@ import { TicketService } from '../../services/ticket.service';
 describe('TicketCheckComponent', () => {
   let component: TicketCheckComponent;
   let fixture: ComponentFixture<TicketCheckComponent>;
-  let ticketServiceMock: any;
+  let ticketServiceMock: jasmine.SpyObj<TicketService>;
 
   beforeEach(async () => {
     ticketServiceMock = jasmine.createSpyObj('TicketService', ['checkTicket']);
@@ -31,58 +31,76 @@ describe('TicketCheckComponent', () => {
   });
 
   it('should initialize the form with empty controls', () => {
+    // given
     const ticketForm = component.ticketForm;
+
+    // then
     expect(ticketForm).toBeTruthy();
     expect(ticketForm.controls['ticketId'].value).toBe('');
     expect(ticketForm.controls['vehicleNumber'].value).toBe('');
   });
 
   it('should have required validators on form controls', () => {
+    // given
     const ticketForm = component.ticketForm;
+
+    // then
     expect(ticketForm.controls['ticketId'].hasError('required')).toBeTruthy();
     expect(ticketForm.controls['vehicleNumber'].hasError('required')).toBeTruthy();
   });
 
   it('should call checkTicket on TicketService when form is valid', () => {
+    // given
     component.ticketForm.controls['ticketId'].setValue('123');
     component.ticketForm.controls['vehicleNumber'].setValue('456');
     ticketServiceMock.checkTicket.and.returnValue(of(true));
 
+    // when
     component.onSubmit();
 
+    // then
     expect(ticketServiceMock.checkTicket).toHaveBeenCalledWith(123, 456);
   });
 
   it('should set isTicketValid to true and isTicketInvalid to false on valid ticket', () => {
+    // given
     component.ticketForm.controls['ticketId'].setValue('123');
     component.ticketForm.controls['vehicleNumber'].setValue('456');
     ticketServiceMock.checkTicket.and.returnValue(of(true));
 
+    // when
     component.onSubmit();
     fixture.detectChanges();
 
+    // then
     expect(component.isTicketValid).toBeTrue();
     expect(component.isTicketInvalid).toBeFalse();
   });
 
   it('should set isTicketValid to false and isTicketInvalid to true on invalid ticket', () => {
+    // given
     component.ticketForm.controls['ticketId'].setValue('123');
     component.ticketForm.controls['vehicleNumber'].setValue('456');
     ticketServiceMock.checkTicket.and.returnValue(of(false));
 
+    // when
     component.onSubmit();
     fixture.detectChanges();
 
+    // then
     expect(component.isTicketValid).toBeFalse();
     expect(component.isTicketInvalid).toBeTrue();
   });
 
   it('should not call checkTicket on TicketService when form is invalid', () => {
+    // given
     component.ticketForm.controls['ticketId'].setValue('');
     component.ticketForm.controls['vehicleNumber'].setValue('');
 
+    // when
     component.onSubmit();
 
+    // then
     expect(ticketServiceMock.checkTicket).not.toHaveBeenCalled();
   });
 });
--- online-municipal-ticket-frontend/src/app/user/components/user-details/user-details.component.spec.ts
+++ online-municipal-ticket-frontend/src/app/user/components/user-details/user-details.component.spec.ts
@@ -1,17 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserDetailsComponent } from './user-details.component';
+import { UserService } from '../../services/user.service';
+import { User } from '../../model/user';
+import { UserRole } from '../../model/user-role';
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
+  let userServiceMock: Partial<UserService>;
 
   beforeEach(async () => {
+    userServiceMock = {
+      getUser: jasmine.createSpy('getUser').and.returnValue({
+        name: 'Michal',
+        lastName: 'Sikacki',
+        email: 'user11@email.com',
+        token: '123456789',
+        role: UserRole.Passenger
+      } as User)
+    };
+
     await TestBed.configureTestingModule({
-      imports: [UserDetailsComponent]
+      imports: [ UserDetailsComponent ],
+      providers: [
+        { provide: UserService, useValue: userServiceMock }
+      ]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +37,16 @@ describe('UserDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set user and userRole correctly', () => {
+    expect(component.user).toEqual({
+      name: 'Michal',
+      lastName: 'Sikacki',
+      email: 'user11@email.com',
+      token: '123456789',
+      role: UserRole.Passenger
+    });
+    expect(component.userRole).toBe('Pasażer');
   });
 });
