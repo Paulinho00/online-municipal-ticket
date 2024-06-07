cdd1a2831d10f4892effaad843931a30a97fb0ab
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2024-06-07 14:30:38 +0200

    Remove service call in view

--- online-municipal-ticket-frontend/src/app/top-bar/top-bar.component.ts
+++ online-municipal-ticket-frontend/src/app/top-bar/top-bar.component.ts
@@ -3,6 +3,7 @@ import { NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user/services/user.service';
 import { response } from 'express';
+import { User } from '../user/model/user';
 
 @Component({
   selector: 'app-top-bar',
@@ -12,7 +13,13 @@ import { response } from 'express';
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
-  constructor( private readonly router: Router, public readonly userService: UserService ){}
+  public currentUser: User | undefined;
+  constructor( private readonly router: Router, private readonly userService: UserService ){
+    this.userService.currentUser.subscribe(res =>
+      {
+        this.currentUser= res;
+      });
+  }
 
   logOut(){
     this.userService.logout().subscribe(response => {
